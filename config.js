bac88bfad51ac1e1928441330a434de12c6465c8
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-09-05 15:53:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,131 +1,19 @@
 //=============『 Utama 』================== //
 global.owner = ['6288295249563','6288294052009'] //ganti pakai nomormu biar bot berfungsi
-global.mods = ['6288295249563','6288294052009'] //ganti pakai nomormu biar bot berfungsi
-global.prems = ['6288295249563','6288294052009'] //ganti pakai nomormu biar bot berfungsi
 
-//=============『 Info Owner 』============== //
-global.nameowner = 'ardhixs'
-global.numberowner = '6288295249563' //ganti pakai nomor mu
-global.instagram = 'https://instagram.com/ardhixs_/' //ganti pakai Instagram mu
-global.github = 'https://github.com/ArdhiXs/' //ganti aja pakai githubmu
-global.sss = '"_"' //jngn ganti hargai creator
-global.yt = 'https://youtube.com/channel/UCeUvY_fIVSAaQ1n5SAylI-A'
-global.dana = '6288295249563' //ganti pakai nomor dana mu
-global.pulsa = '6288295249563' //ganti pakai nomor pulsa mu
-global.gopay = 'GX ada' //ganti pakai nomor Gopay mu 
-global.saweria = '"_"' //Ganti link saweria mu
-
-//=============『 Info Bot 』=================//
-global.namebot = 'aXs BOT-MD' //ganti jadiin nama bot mu
-global.gc = '"_"' //ganti pakai grup mu
-global.apikeyar = 'ardhixs.herokuapp.com'
-global.nmrardi = '+6288294052009'
-global.web = 'https://www.instagram.com/ardhixs_/' //ubah jadi website mu
-global.nomer = 'https://wa.me/6288294052009' //ganti jadi nomor mu
-global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERMANEN Rp 10.000\nMAKSIMAL 2 GRUP\n》PERMANEN  Rp 20.000\nMAKSIMAL 6 GRUP \n》1 BULAN Rp 5.000\nMAKSIMAL 1 GRUP'
-
-//=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://i.ibb.co/1RRbRYY/thumb.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://i.ibb.co/1RRbRYY/thumb.jpg' //ini juga ganti pakai foto bot mu
-global.fotonya3 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya4 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya5 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya6 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini ganti pakai foto sosialmedia mu
-global.fotonya7 = 'https://telegra.ph/file/5da3cd5d775775f5e8791.jpg' //ini juga ganti
-global.fotonya8 = 'https://telegra.ph/file/14ec0e0c93af4812d1fd7.jpg' //foto session
-global.donasi = 'https://telegra.ph/file/c171460c1e60d22ea379e.jpg' //ganti pakai poto payment atau apa
-global.axs30 = 'HxRqMLZX'
-global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
-global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '_@ardhixs__'
-global.watermark = wm
-global.wm2 = '兰｢ȺɍđħɨXs｣兰'
-global.wm3 = '@ardhixs_'
-global.htki = '––––––『' 
-global.htka = '』––––––'
-global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
-global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wait = '⏳Tunggu kurang lebih 1 menit karna sedang diproses. . .'
-global.eror = '_*🚨 Server Bot Sedang Error*_'
-global.benar = 'Benar ✅\n'
-global.salah = 'Salah ❌\n'
-global.stiker_wait = '⏳ Stiker sedang Proses'
+//=============『 WM STICKER 』============== //
 global.packname = '卂尺刀卄工'
 global.author = '\n\n\n\n\n\n\n\n@ardhixs_'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
   // name: 'https://website'
-  amel: 'https://melcanz.com',
-    bx: 'https://bx-hunter.herokuapp.com',
-  dhnjing: 'https://dhnjing.xyz',
-  hardianto: 'https://hardianto-chan.herokuapp.com',
-  jonaz: 'https://jonaz-api-v2.herokuapp.com',
-  neoxr: 'https://neoxr-api.herokuapp.com',
   nrtm: 'https://nurutomo.herokuapp.com',
-  xteam: 'https://api.xteam.xyz',
-  nzcha: 'http://nzcha-apii.herokuapp.com',
-  bg: 'http://bochil.ddns.net',
-  fdci: 'https://api.fdci.se',
-  dzx: 'https://api.dhamzxploit.my.id',
-  bsbt: 'https://bsbt-api-rest.herokuapp.com',
-  zahir: 'https://zahirr-web.herokuapp.com',
-  zeks: 'https://api.zeks.xyz',
-  zekais: 'http://zekais-api.herokuapp.com',
-  hardianto: 'https://hardianto-chan.herokuapp.com',
-  pencarikode: 'https://pencarikode.xyz', 
-  erdwepe: 'https://erdwpe-api.herokuapp.com',
-  lolhuman: 'https://api.lolhuman.xyz',
-  LeysCoder: 'https://leyscoders-api.herokuapp.com',
-  rey: 'https://server-api-rey.herokuapp.com'
+  xteam: 'https://api.xteam.xyz'
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://melcanz.com': 'elaina',
-  'https://server-api-rey.herokuapp.com': 'apirey',
   'https://api.xteam.xyz': 'd37372311698ed1d',
-  'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://bsbt-api-rest.herokuapp.com': 'benniismael',
-  'https://api.zeks.xyz': 'apivinz',
-  'https://hardianto-chan.herokuapp.com': 'hardianto',
-  'https://pencarikode.xyz': 'pais', 
-  'https://leyscoders-api.herokuapp.com': 'dappakntlll',
-  'https://zekais-api.herokuapp.com': 'apikeymu',
-  'https://api.lolhuman.xyz': 'Deffbotz',
-}
-
-//=============『 RPG GAMES 』================== //
-global.multiplier = 69 // The higher, The harder levelup
-global.rpg = {
-  emoticon(string) {
-    string = string.toLowerCase()
-    let emot = {
-      exp: '✉️',
-      money: '💵',
-      potion: '🥤',
-      diamond: '💎',
-      common: '📦',
-      uncommon: '🎁',
-      mythic: '🗳️',
-      legendary: '🗃️',
-      pet: '🎁',
-      sampah: '🗑',
-      armor: '🥼',
-      sword: '⚔️',
-      kayu: '🪵',
-      batu: '🪨',
-      string: '🕸️',
-      kuda: '🐎',
-      kucing: '🐈' ,
-      anjing: '🐕',
-      petFood: '🍖',
-      gold: '👑',
-      emerald: '💚'
-    }
-    let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
-    if (!results.length) return ''
-    else return emot[results[0][0]]
-  }
 }
 
 //===========『 Jangan Di Ubah 』================ //
